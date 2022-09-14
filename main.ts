e7c70102b4dd26a74c5acc61072bd02cba3763e6
runandbike <112710739+runandbike@users.noreply.github.com>
2022-09-14 16:57:56 +0200

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,7 @@
+import { initMetaDataAccount, createMetaData, updateMetaData } from './src/index';
+
+/**
+ * Main script run for testing purposes
+ * createMetaData not complete but will overwrite update init with args
+ */
+initMetaDataAccount();
