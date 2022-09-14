824705ce6679fe4b31836c7fbd6b3b491e51b7d0
runandbike <112710739+runandbike@users.noreply.github.com>
2022-09-14 17:02:34 +0200

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { updateMetaData } from './updateToken';
+import { createMetaData } from './createAccount';
+import { initMetaDataAccount } from './initial';
+
+export { updateMetaData, initMetaDataAccount, createMetaData }
